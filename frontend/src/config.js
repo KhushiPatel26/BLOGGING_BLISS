0c269b3c945da74949264af0deb498555759d6e5
KhushiPatel26 <pkhushi1263@gmail.com>
2025-03-05 22:46:47 +0530

    Update

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 
-const API_BASE_URL = "https://blogging-bliss.onrender.com/api";//https://your-backend.up.railway.app/";
+const API_BASE_URL = "bloggingbliss-production.up.railway.app";//"https://blogging-bliss.onrender.com/api";//https://your-backend.up.railway.app/";
 
 export default API_BASE_URL;
